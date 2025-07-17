39a9e3e18b37cf3f1d5d8a0235ac1b83f37d121d
Tomas Diez <tomasdiez@gmail.com>
2025-07-17 08:15:55 +0800

    Update page.tsx

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -1,3 +1,76 @@
+import Layout from '@/components/Layout'
+import { load } from 'outstatic/server'
+import ContentGrid from '@/components/ContentGrid'
+import markdownToHtml from '@/lib/markdownToHtml'
+
+export default async function Index() {
+  const { content, allPosts, allProjects } = await getData()
+
+  return (
+    <Layout>
+      <div className="max-w-6xl mx-auto px-5">
+        <section className="mt-16 mb-16 md:mb-12">
+          <div
+            className="prose lg:prose-2xl home-intro"
+            dangerouslySetInnerHTML={{ __html: content }}
+          />
+        </section>
+        {allPosts.length > 0 && (
+          <ContentGrid
+            title="Posts"
+            items={allPosts}
+            collection="posts"
+            priority
+          />
+        )}
+        {allProjects.length > 0 && (
+          <ContentGrid
+            title="Projects"
+            items={allProjects}
+            collection="projects"
+          />
+        )}
+      </div>
+    </Layout>
+  )
+}
+
+async function getData() {
+  const db = await load()
+
+  const page = await db
+    .find({ collection: 'pages', slug: 'home' }, ['content'])
+    .first()
+
+  const content = await markdownToHtml(page.content)
+
+  const allPosts = await db
+    .find({ collection: 'posts' }, [
+      'title',
+      'publishedAt',
+      'slug',
+      'coverImage',
+      'description',
+      'tags'
+    ])
+    .sort({ publishedAt: -1 })
+    .toArray()
+
+  const allProjects = await db
+    .find({ collection: 'projects' }, ['title', 'slug', 'coverImage'])
+    .sort({ publishedAt: -1 })
+    .toArray()
+
+  return {
+    content,
+    allPosts,
+    allProjects
+  }
+}
+
+
+
+/*
 // src/app/(web)/page.tsx
 
 // Assuming Outstatic is configured correctly in your project (e.g., in a lib/outstatic.ts)
@@ -172,3 +245,4 @@ export default async function HomePage() {
 // You might want to consider `revalidatePath` or `revalidateTag` if you want
 // more explicit control over revalidation without a full rebuild on content changes.
 // Learn more: https://nextjs.org/docs/app/building-your-application/data-fetching/revalidating
+*/
